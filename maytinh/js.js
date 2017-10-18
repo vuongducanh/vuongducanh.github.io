bbaff31cf8ef435987229295d3efced4455dfe95
vuongducanh <31908824+vuongducanh@users.noreply.github.com>
2017-10-18 21:54:37 +0700

    u4yuf
    ưedfwef

--- maytinh/js.js
+++ maytinh/js.js
@@ -1,8 +1,20 @@
-function a(id){
-	return document.getElementById('id');
-}
-var x = "";
-function set_num(num){
-	x= x + num;
-	a('div').value = x;
-}
+
+     
+     
+      
+     
+      
+     
+     
+
+
+     
+     
+     
+     
+     
+     
+    
+
+
+      
