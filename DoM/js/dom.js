ef6e9c9e4e08882a08ffda9ad4ee47803a631dd8
vuongducanh <31908824+vuongducanh@users.noreply.github.com>
2017-10-18 15:27:15 +0700

    đenjen

---
+++ DoM/js/dom.js
@@ -0,0 +1,16 @@
+
+function  changeColor(){
+	var x= document.getElementById("p1").style.color = "blue";
+	var x= document.getElementById("p2").style.color = "yellow";
+	 var x= document.getElementById("p3").style.color = "red";
+	return x;
+
+}
+function changeBgColor(){
+	var y= document.body.style.backgroundColor = "red";
+	return y;
+}
+function chu(a){
+	
+ ;
+}
