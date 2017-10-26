0e199a420e95efd36b02abf613ea9f18a7e50def
vuongducanh <31908824+vuongducanh@users.noreply.github.com>
2017-10-26 15:05:09 +0700

    baixong

--- login đăng nhập/js/ketqua.js
+++ login đăng nhập/js/ketqua.js
@@ -3,10 +3,12 @@ var url = new URL(window.location.href);
 
 var searchParams = new URLSearchParams(url.search);
 
-$("#ten").html("tên : "+searchParams.get("ten"));
-$("#Email").html("email : "+searchParams.get("Email"));
-$("#password").html("pasword : "+searchParams.get("password"));
-$("#năm").html(" : "+searchParams.get("year"));
-$("#ngày").html(": "+searchParams.get("date"));
-$("#tháng").html(" : "+searchParams.get("thang"));
-$("#remenber").html("giới tinh : "+searchParams.get("remenber"));
+$("#ten").html("Tên : "+searchParams.get("ten"));
+$("#Email").html("Email : "+searchParams.get("Email"));
+$("#password").html("Pasword : "+searchParams.get("password"));
+$("#năm").html("Năm : "+searchParams.get("year"));
+$("#ngày").html("Ngày : "+searchParams.get("date"));
+$("#tháng").html("Tháng : "+searchParams.get("thang"));
+$("#remenber").html("Giới tinh : "+searchParams.get("remenber"));
+$("#sống").html("Nơi ở : "+searchParams.get("sống"));
+
