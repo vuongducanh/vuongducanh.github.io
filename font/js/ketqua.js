843b4ac3df77a2d700fc1ec983d52a77677e8f53
vuongducanh <31908824+vuongducanh@users.noreply.github.com>
2017-10-27 14:35:49 +0700

    hazz

--- font/js/ketqua.js
+++ font/js/ketqua.js
@@ -3,12 +3,10 @@ var url = new URL(window.location.href);
 
 var searchParams = new URLSearchParams(url.search);
 
-$("#ten").html("Tên : "+searchParams.get("ten"));
+$("#ten").html("Họ và Tên : "+searchParams.get("ten"));
 $("#Email").html("Email : "+searchParams.get("Email"));
-$("#password").html("Pasword : "+searchParams.get("password"));
-$("#năm").html("Năm : "+searchParams.get("year"));
-$("#ngày").html("Ngày : "+searchParams.get("date"));
-$("#tháng").html("Tháng : "+searchParams.get("thang"));
+$("#password").html("Mật khẩu : "+searchParams.get("password"));
+$("#ngày").html("Năm tháng ngày sinh : "+searchParams.get("sinh"));
+$("#song").html("Quê quán : "+searchParams.get("song"));
 $("#remenber").html("Giới tinh : "+searchParams.get("remenber"));
-$("#sống").html("Nơi ở : "+searchParams.get("sống"));
 
