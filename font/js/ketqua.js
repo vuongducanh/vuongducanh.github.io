881d0c1acf2b3db80987f769701319bc92228698
vuongducanh <31908824+vuongducanh@users.noreply.github.com>
2017-10-30 09:41:48 +0700

    nhì nhăng lăm

--- font/js/ketqua.js
+++ font/js/ketqua.js
@@ -4,6 +4,9 @@ var url = new URL(window.location.href);
 var searchParams = new URLSearchParams(url.search);
 
 $("#ten").html("Họ và Tên : "+searchParams.get("ten"));
+
+// get("ten") là lấy name ở trang chủ trang chủ đăng kí rồi tên sẽ truền vào id cua trang ketqua.html
+
 $("#Email").html("Email : "+searchParams.get("Email"));
 $("#password").html("Mật khẩu : "+searchParams.get("password"));
 $("#ngày").html("Năm tháng ngày sinh : "+searchParams.get("sinh"));
