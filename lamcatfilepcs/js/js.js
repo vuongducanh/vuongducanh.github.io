ac8455cce61716fcbb6fbac0f8d3fba94c9b55c3
vuongducanh <anhvuong730@gmail.com>
2017-11-27 01:40:06 +0700

    bài tập an báccjh

--- lamcatfilepcs/js/js.js
+++ lamcatfilepcs/js/js.js
@@ -1,7 +1,5 @@
 function  sotien(){
-		 var a =Math.ceil((2 * (document.getElementById('input1').value) /Math.log2(document.getElementById('input1').value )))
-		 console.log(a);
-   	     document.getElementById('ketquasinhvien').innerHTML = a +"<br>"+ "triệu vnd" ;
-
-
+		 var a = Math.ceil((2 * (document.getElementById('input1').value) /Math.log2(document.getElementById('input1').value )))
+   	    document.getElementById('ketquasinhvien').innerHTML = a +"<br>"+ "triệu vnd" ;
+      
 }
