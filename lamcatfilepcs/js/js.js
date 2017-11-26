0c9ea99cf49ea2b64dbb34e82ec31b86cb31f3b6
vuongducanh <anhvuong730@gmail.com>
2017-11-27 00:45:57 +0700

    12

---
+++ lamcatfilepcs/js/js.js
@@ -0,0 +1,7 @@
+function  sotien(){
+		 var a =Math.ceil((2 * (document.getElementById('input1').value) /Math.log2(document.getElementById('input1').value )))
+		 console.log(a);
+   	     document.getElementById('ketquasinhvien').innerHTML = a +"<br>"+ "triệu vnd" ;
+
+
+}
