0ef2df309e15b41a08234c50d9b8e6fbdb69d052
vuongducanh <anhvuong730@gmail.com>
2017-11-27 19:50:10 +0700

    anh

--- lamcatfilepcs/js/js.js
+++ lamcatfilepcs/js/js.js
@@ -1,5 +1,6 @@
 function  sotien(){
 		 var a = Math.ceil((2 * (document.getElementById('input1').value) /Math.log2(document.getElementById('input1').value )))
-   	    document.getElementById('ketquasinhvien').innerHTML = a +"<br>"+ "triệu vnd" ;
+
+   	     document.getElementById('inraketqua').innerHTML = a  ;
       
 }
