5443547fbafcc6ff3b9bd41fc7e37f7114c8574d
vuongducanh <anhvuong730@gmail.com>
2017-11-29 00:13:57 +0700

    sua xong

--- project.com/jsabout/about.js
+++ project.com/jsabout/about.js
@@ -1,8 +1,7 @@
 $(function(){
-     
-
      $(window).on('scroll',function(){
-     	var a = $('#contact')[0].getBoundingClientRect().top;   //lấy thông số đối tượng đấy ;
+     	var a = $('#contact')[0].getBoundingClientRect().top;   //lấy thông số đối tượng đấy trái, trên, rộng và chiều cao.
+ 
         var b  = $(window).innerHeight();     //lấy chiều cao của cửa sổ hiện tại;
      	 
            if(a < b-100){
@@ -16,4 +15,35 @@ $(function(){
      });
 });
 
-// do you have a girl fren
+$(function(){
+     $(window).on('scroll',function(){
+      var a = $('.search-wrapper')[0].getBoundingClientRect().top;   //lấy thông số đối tượng đấy trái, trên, rộng và chiều cao.
+ 
+        var b  = $(window).innerHeight();     //lấy chiều cao của cửa sổ hiện tại;
+       
+           if(a < b-10){
+             $('.search-wrapper').css( {
+              "opacity" : "1" ,
+              "transition":"all 1s ease",
+              "transform": "translateX(0)"
+             })
+           }
+
+     });
+});
+// hết phần js cho hiệu ứng anomation
+
+function searchToggle(obj, evt){
+    var container = $(obj).closest('.search-wrapper');
+        if(!container.hasClass('active')){
+            container.addClass('active');
+            evt.preventDefault();
+        }
+        else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+            container.removeClass('active');
+            // clear input
+            container.find('.search-input').val('');
+        }
+}
+
+
