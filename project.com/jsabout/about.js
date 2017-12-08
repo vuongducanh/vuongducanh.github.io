cfd0f5687f4480a9dafb1e5f2055a3e875ac8806
vuongducanh <anhvuong730@gmail.com>
2017-12-08 23:47:07 +0700

    cv
    v

--- project.com/jsabout/about.js
+++ project.com/jsabout/about.js
@@ -1,22 +1,53 @@
-$(function(){
-     $(window).on('scroll',function(){
-     	var a = $('#contact')[0].getBoundingClientRect().top;   //lấy thông số đối tượng đấy trái, trên, rộng và chiều cao.
- 
-        var b  = $(window).innerHeight();     //lấy chiều cao của cửa sổ hiện tại;
-     	 
-           if(a < b-100){
-           	 $('#contact').css( {
-           	 	"opacity" : "1" ,
-           	 	"transition":"all 1s ease",
-           	 	"transform": "translateX(0)"
-           	 })
-           }
+$(function () {
+	$(window).on('scroll', function () {
+		var a = $('#contact')[0].getBoundingClientRect().top;   //lấy thông số đối tượng đấy trái, trên, rộng và chiều cao.
 
-     });
+		var b = $(window).innerHeight();     //lấy chiều cao của cửa sổ hiện tại;
+
+		if (a < b - 100) {
+			$('#contact').css({
+				"opacity": "1",
+				"transition": "all 1s ease",
+				"transform": "translateX(0)"
+			})
+		}
+
+	});
 });
 
 // hết phần animation cho khối đăng nhập và nhắn tin
 
+
+$(document).on('click', '.them', function (e) {
+	e.preventDefault();
+	var parent = $(this).parents('.img-box');
+	var cart = $(document).find('#cart-shop');
+	var src = parent.find('img').attr('src');
+	
+	var partop = parent.offset().top;
+	var parleft = parent.offset().left;
+	
+	
+	$('<img />', {
+		class: 'img-fly',
+		src: src
+	}).appendTo('body').css({
+		'top':partop,
+		'left':parleft + parent.width()-50
+	});
+	setTimeout(function(){
+       $(document).find('.img-fly').css({
+			'top':cart.offset().top,
+			'left':cart.offset().left
+	   });
+	setTimeout(function(){
+		$(document).find('.img-fly').remove();
+		var citem  =parseInt( $('#count-item').attr('value'))+1;
+		$('#count-item').html(citem);
+		citem++;
+	},1000)
+	},500);
+});
 
 
 
