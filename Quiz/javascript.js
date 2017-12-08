31fb2d35cb9747467c92d40d0fcf781aeccd4d73
vuongducanh <anhvuong730@gmail.com>
2017-12-08 20:18:10 +0700

    hh89h

---
+++ Quiz/javascript.js
@@ -0,0 +1,61 @@
+
+var cauhoi=[
+    {
+        "question" : " Nếu chỉ có một que diêm, trong một ngày mùa đông giá rét, bạn bước vào căn phòng có một cây đèn, một bếp dầu, và một bếp củi, bạn thắp gì trước tiên??",
+        "option1" : "Cây đèn",
+        "option2" : "Bếp dầu",
+        "option3" : "Bếp củi",
+        "option4" : "Que diêm",
+        "dung" : "Que diêm",
+    },
+    {
+        "question" : "2 con vịt đi trước 2 con vịt, 2 con vịt đi sau 2 con vịt, 2 con vịt đi giữa 2 con vịt. Hỏi có mấy con vịt??",
+        "option1" : "1",
+        "option2" : "3",
+        "option3" : "2",
+        "option4" : "4",
+        "dung" : "4",
+    },
+    {
+        "question" : " Lịch nào dài nhất??",
+        "option1" : "lich Sử",
+        "option2" : "Tờ lịch",
+        "option3" : "không có",
+        "option4" : "lịch sự",
+        "dung" : "Lich sử",
+    },
+    {
+        "question" : "Con đường dài nhất là đường nào?",
+        "option1" : "Đường ô tô ",
+        "option2" : "Đường đời",
+        "option3" : "Đường thủy",
+        "option4" : "Đường không",
+        "dung" : "Đường đời",
+    },
+    {
+        "question" : "10-4*4/3+9=?",
+        "option1" : "5",
+        "option2" : "6",
+        "option3" : "5",
+        "option4" : "17",
+        "dung" : "17",
+    },
+    {
+        "question" : "4-5/3+5*4*3/100=?",
+        "option1" : "0.3",
+        "option2" : "0.56",
+        "option3" : "1",
+        "option4" : "4",
+        "dung" : "0.56",
+    },
+    {
+        "question" : "10*3-6/4= ?",
+        "option1" : "4",
+        "option2" : "2",
+        "option3" : "6",
+        "option4" : "9",
+        "dung" : "4",
+    }
+];
+
+
