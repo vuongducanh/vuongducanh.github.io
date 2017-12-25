e46d56867825eef17520c2b14f72f8d26e11446f
vuongducanh <anhvuong730@gmail.com>
2017-12-26 01:15:40 +0700

    đi nghủ
    đi

--- project1.com/js-index/index.js
+++ project1.com/js-index/index.js
@@ -1,35 +1,4 @@
 
-// js cho them vao gio hang
-$(document).on('click', '.them', function (e) {
-	e.preventDefault();
-	var parent = $(this).parents('.img-box');
-	var cart = $(document).find('#cart-shop');
-	var src = parent.find('img').attr('src');
-	var partop = parent.offset().top;
-	var parleft = parent.offset().left;
-	
-	
-	$('<img />', {
-		class: 'img-fly',
-		src: src
-	}).appendTo('body').css({
-		'top':partop,
-		'left':parleft + parent.width()-50
-	});
-	setTimeout(function(){
-       $(document).find('.img-fly').css({
-			'top':cart.offset().top,
-			'left':cart.offset().left
-	   });
-	setTimeout(function(){
-		$(document).find('.img-fly').remove();
-		var citem  =parseInt( $('#count-item').attr('value'))+1;
-		$('#count-item').html(citem);
-		citem++;
-	},1000)
-	},500);
-});
-
 
 
 // js cho back-to-top
@@ -93,7 +62,6 @@ $(document).ready(function() {
 			
 			tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
 			tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
-			
 			g.preventDefault();
 		} );
 	})(jQuery);
@@ -103,4 +71,58 @@ $(document).ready(function() {
 
 // js cho appento thêm vao vào giỏ hàng
 //$('.money-products-js').append('<div data-toggle="buttons"><label class="btn btn-lg btn-block btn-success text-success active"> <input type="radio" name="options" id="option1" autocomplete="off" checked> <i class="fa fa-check-circle-o animated fadeIn"></i> <br /> đã thêm </label> <label class="btn btn-lg btn-block btn-default text-default"> <input type="radio" name="options" id="option2" autocomplete="off"> <i class="fa fa-circle-o "></i> <br /> Thêm giỏ hàng </label> </div>')
-$('.money-products-js').append( '<button class="btn btn-danger my-cart-btn" data-id="1" data-name="product 1" data-summary="summary 1" data-price="10" data-quantity="1" data-image="images/img_1.png">Thêm giỏ hàng</button>')
+$('.money-products-js').append('<del>&nbsp;&nbsp;150000đ</del>').append( '<button class="btn btn-danger my-cart-btn" data-id="1" data-name="product 1" data-summary="summary 1" data-price="10" data-quantity="1" data-image="img/giaynam1.jpg">Thêm giỏ hàng</button>')
+
+$(function () {
+    var goToCartIcon = function($addTocartBtn){
+      var $cartIcon = $(".my-cart-icon");
+      var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+      $addTocartBtn.prepend($image);
+      var position = $cartIcon.position();
+      $image.animate({
+        top: position.top,
+        left: position.left
+      }, 500 , "linear", function() {
+        $image.remove();
+      });
+    }
+
+    $('.my-cart-btn').myCart({
+      currencySymbol: '$',
+      classCartIcon: 'my-cart-icon',
+      classCartBadge: 'my-cart-badge',
+      classProductQuantity: 'my-product-quantity',
+      classProductRemove: 'my-product-remove',
+      classCheckoutCart: 'my-cart-checkout',
+      affixCartIcon: true,
+      showCheckoutModal: true,
+      numberOfDecimals: 2,
+      cartItems: [
+        {id: 1, name: 'product 1', summary: 'summary 1', price: 1000, quantity: 1, image: 'img/do-nam1.jpg'},
+        {id: 2, name: 'product 2', summary: 'summary 2', price: 2000, quantity: 2, image: 'img/du-nu2.jpg'},
+        {id: 3, name: 'product 3', summary: 'summary 3', price: 3000, quantity: 1, image: 'img/do-nu4.jpg'}
+      ],
+      clickOnAddToCart: function($addTocart){
+        goToCartIcon($addTocart);
+      },
+      afterAddOnCart: function(products, totalPrice, totalQuantity) {
+        console.log("afterAddOnCart", products, totalPrice, totalQuantity);
+      },
+      clickOnCartIcon: function($cartIcon, products, totalPrice, totalQuantity) {
+        console.log("cart icon clicked", $cartIcon, products, totalPrice, totalQuantity);
+      },
+      checkoutCart: function(products, totalPrice, totalQuantity) {
+        var checkoutString = "Total Price: " + totalPrice + "\nTotal Quantity: " + totalQuantity;
+        checkoutString += "\n\n id \t name \t summary \t price \t quantity \t image path";
+        $.each(products, function(){
+          checkoutString += ("\n " + this.id + " \t " + this.name + " \t " + this.summary + " \t " + this.price + " \t " + this.quantity + " \t " + this.image);
+        });
+        alert(checkoutString)
+        console.log("checking out", products, totalPrice, totalQuantity);
+      },
+      getDiscountPrice: function(products, totalPrice, totalQuantity) {
+        console.log("calculating discount", products, totalPrice, totalQuantity);
+        return totalPrice * 0.5;
+      }
+    });
+  });
