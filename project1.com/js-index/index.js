af2560478bab674fc6d5f44a9d29c2c24e041465
vuongducanh <anhvuong730@gmail.com>
2017-12-27 00:49:23 +0700

    đi ngủ

--- project1.com/js-index/index.js
+++ project1.com/js-index/index.js
@@ -72,8 +72,9 @@ $(document).ready(function() {
 
 
 // js cho appento thêm vao vào giỏ hàng
-//$('.money-products-js').append('<div data-toggle="buttons"><label class="btn btn-lg btn-block btn-success text-success active"> <input type="radio" name="options" id="option1" autocomplete="off" checked> <i class="fa fa-check-circle-o animated fadeIn"></i> <br /> đã thêm </label> <label class="btn btn-lg btn-block btn-default text-default"> <input type="radio" name="options" id="option2" autocomplete="off"> <i class="fa fa-circle-o "></i> <br /> Thêm giỏ hàng </label> </div>')
-$('.money-products-js').append('<del>&nbsp;&nbsp;150000đ</del>').append( '<button class="btn btn-danger my-cart-btn" data-id="1" data-name="product 1" data-summary="summary 1" data-price="10" data-quantity="1" data-image="img/giaynam1.jpg">Thêm giỏ hàng</button>')
+
+  $('.money-products-js').append('<del>&nbsp;&nbsp;150000đ</del>').append('<div data-toggle="buttons"><label class="btn btn-lg btn-block btn-success text-success active"> <input type="radio" name="options" id="option1" autocomplete="off" checked> <i class="fa fa-check-circle-o animated fadeIn"></i> <br /> đã thêm </label> <label class="btn btn-lg btn-block btn-default text-default"> <input type="radio" name="options" id="option2" autocomplete="off"> <i class="fa fa-circle-o "></i> <br /> Thêm giỏ hàng </label> </div>')
+//$(" .alternate-details").append('<button class="btn btn-danger my-cart-btn" data-id="1" data-name="product 1" data-summary="summary 1" data-price="10" data-quantity="1" data-image="img/giaynam1.jpg">Thêm giỏ hàng</button>')
 
 $(function () {
     var goToCartIcon = function($addTocartBtn){
@@ -100,18 +101,18 @@ $(function () {
       showCheckoutModal: true,
       numberOfDecimals: 2,
       cartItems: [
-        {id: 1, name: 'product 1', summary: 'summary 1', price: 1000, quantity: 1, image: 'img/do-nam1.jpg'},
-        {id: 2, name: 'product 2', summary: 'summary 2', price: 2000, quantity: 2, image: 'img/du-nu2.jpg'},
-        {id: 3, name: 'product 3', summary: 'summary 3', price: 3000, quantity: 1, image: 'img/do-nu4.jpg'}
+        {id: 1, name: 'product 1', summary: 'summary 1', price: 10, quantity: 1, image: 'img/do-nam1.jpg'},
+        {id: 2, name: 'product 2', summary: 'summary 2', price: 20, quantity: 2, image: 'img/do-nam2.jpg'},
+        {id: 3, name: 'product 3', summary: 'summary 3', price: 30, quantity: 1, image: 'img/do-nam3.jpg'}
       ],
       clickOnAddToCart: function($addTocart){
         goToCartIcon($addTocart);
       },
       afterAddOnCart: function(products, totalPrice, totalQuantity) {
-        console.log("afterAddOnCart", products, totalPrice, totalQuantity);
+        
       },
       clickOnCartIcon: function($cartIcon, products, totalPrice, totalQuantity) {
-        console.log("cart icon clicked", $cartIcon, products, totalPrice, totalQuantity);
+       
       },
       checkoutCart: function(products, totalPrice, totalQuantity) {
         var checkoutString = "Total Price: " + totalPrice + "\nTotal Quantity: " + totalQuantity;
@@ -120,10 +121,9 @@ $(function () {
           checkoutString += ("\n " + this.id + " \t " + this.name + " \t " + this.summary + " \t " + this.price + " \t " + this.quantity + " \t " + this.image);
         });
         alert(checkoutString)
-        console.log("checking out", products, totalPrice, totalQuantity);
       },
       getDiscountPrice: function(products, totalPrice, totalQuantity) {
-        console.log("calculating discount", products, totalPrice, totalQuantity);
+       
         return totalPrice * 0.5;
       }
     });
--- project1.com/js/jquery.mycart.js
+++ project1.com/js/jquery.mycart.js
@@ -215,7 +215,7 @@
         '<div class="modal-content">' +
         '<div class="modal-header">' +
         '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
-        '<h4 class="modal-title" id="myModalLabel"><span class="glyphicon glyphicon-shopping-cart"></span> My Cart</h4>' +
+        '<h4 class="modal-title" id="myModalLabel"><span class="glyphicon glyphicon-shopping-cart"></span> Giỏ hàng</h4>' +
         '</div>' +
         '<div class="modal-body">' +
         '<table class="table table-hover table-responsive" id="' + idCartTable + '"></table>' +
