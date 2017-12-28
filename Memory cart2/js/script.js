447af25ae0166d7a99790659e3e4879d97090de0
vuongducanh <anhvuong730@gmail.com>
2017-12-28 22:56:00 +0700

    cgh
    cg

---
+++ Memory cart2/js/script.js
@@ -0,0 +1,46 @@
+  $(document).ready(function () {
+  	var cards = ['f1', 'f2', 'f3', 'f4', 'f5'];
+  	var current=null;
+
+
+  	function shuffle(array) {
+  		var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+$(function(){
+	// nhan dôi mảng
+	cards=cards.concat(cards); 	
+		// dảo vị trí các con bài
+		cards=shuffle(cards);
+		var html='';
+		for(var i = 0 ;i<cards.length;i++){
+			html+= '<div class="content"> <div class="grid"> <div class="card" data-name="'+cards[i]+'"> <div class="font" > <img src="img/anhsau1.jpg"> </div> <div class="back" > <img src="img/'+cards[i]+'.jpg"> </div> </div> </div> </div> ';
+		};
+		$('.content').html(html);
+
+		$('.card').on('click',function(){
+			$(this).addClass('flip');
+			
+		});
+	});
+
+})
+
+// console.log("dthdg");
+	// $(this).css('pointer-events','none');
